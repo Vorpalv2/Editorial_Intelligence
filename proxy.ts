755170cc1d5ac8d8bc5a8300eb265dd93e90f0ee
Aakash Anand <aakashanand1995@gmail.com>
2026-04-28 20:17:02 +0530

    updated middleware

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 // 1. Define your matchers
-const isProtectedRoute = createRouteMatcher(["/history(.*)", "/setting", "/"]);
+const isProtectedRoute = createRouteMatcher(["/history(.*)", "/setting"]);
 const isPublicAuthRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
