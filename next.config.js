e27641f919e16d8f8cddfa76bd71e98387e9287c
Aakash Anand <aakashanand1995@gmail.com>
2026-04-29 09:31:08 +0530

    nextconfig update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ["@sparticuz/chromium"],
   env: {
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   },
